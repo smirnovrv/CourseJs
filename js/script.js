6dae454eee94b5b4683942de1944b503b4e5d93f
Roman <Smirnofrv@yandex.ru>
2022-05-19 11:11:23 +0500

    js class 47_2

--- js/script.js
+++ js/script.js
@@ -91,21 +91,71 @@
 // }
 
 //урок 47 рекурсия
-function pow(x, n){
-    let result=1;
-     for (let i=0; i<n; i++){
-        result *=x;
-     }
-     return result;
+// function pow(x, n){
+//     let result=1;
+//      for (let i=0; i<n; i++){
+//         result *=x;
+//      }
+//      return result;
+// }
+
+// function pow(x, n){
+//    if (n===1){
+//       return x;
+//    } else {
+//       return x*pow(x, n-1);
+//    }
+// }
+
+// console.log(pow(2,2)); 
+// console.log(pow(2,3)); 
+
+let students = {
+    js: [{
+        name: 'john',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
+
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }]
+    }
+};
+
+function getTotalProgressByIteration(data){
+    let total = 0;
+    let students = 0;
+
+    for(let course of Object.values(data)){
+        if(Array.isArray(course)){
+            students += course.length;        
+            for (let i=0; i<course.length; i++){
+                total += course[i].progress;
+            }
+        } else {
+            for(let subcourse of Object.values(course)){
+                students += subcourse.length;
+                for (let i=0; i<subcourse.length; i++){
+                    total += subcourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total/students;
 }
 
-function pow(x, n){
-   if (n===1){
-      return x;
-   } else {
-      return x*pow(x, n-1);
-   }
-}
-
-console.log(pow(2,2)); 
-console.log(pow(2,3)); 
+console.log(getTotalProgressByIteration(students));
